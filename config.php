d49ea99e7edd844bcfd006b1164b48108b7eb32b
WaleedMuslem <waleedmosllm2020@gmail.com>
2023-02-21 14:47:11 +0100

    feat<config.php>:create config file

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+define('BASE_PATH', __DIR__);
+define('BASE_URL', 'http://localhost/WaleedShop/demo/');
+
+define('DB_HOST', 'localhost');
+define('DB_PORT', 3306);
+define('DB_NAME', 'WaleedShop');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+
+define('PAGE_LIMIT', 12);
+
+
+define('MAX_UPLOAD_SIZE', 10);
