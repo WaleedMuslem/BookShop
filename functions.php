64ca1f5f2d5dd1ecb1171737d1cf85e91f8fe3fa
WaleedMuslem <waleedmosllm2020@gmail.com>
2023-02-21 14:36:41 +0100

    feat<functions.php>:create current_url function

--- functions.php
+++ functions.php
@@ -143,3 +143,17 @@ function guest_only() {
         redirect('home');
     }
 }
+
+function current_url() {
+    if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
+         $url = "https://";
+    else
+         $url = "http://";
+    // Append the host(domain name, ip) to the URL.
+    $url.= $_SERVER['HTTP_HOST'];
+
+    // Append the requested resource location to the URL
+    $url.= $_SERVER['REQUEST_URI'];
+
+    return $url;
+}
