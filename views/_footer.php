9544ea829d08b602822603cf3cb75603c4a26b87
WaleedMuslem <waleedmosllm2020@gmail.com>
2023-02-25 00:39:26 +0100

    feat<_footer.php>: create footer view

---
+++ views/_footer.php
@@ -0,0 +1,16 @@
+</main>
+<footer>
+        <div class="container-footer">
+          <h1 class="copyright">Copyright © 2023 WaleedShop</h1>
+          <h5>SOCIAL MEDIA</h5>
+          <div class="social-icons">
+            <a href="#"><img src="<?= asset("img/icons/facebook.png"); ?>" alt="Facebook"></a>
+            <a href="#"><img src="<?= asset("img/icons/gmail.png"); ?>" alt="Gmail"></a>
+            <a href="#"><img src="<?= asset("img/icons/instagram.png"); ?>" alt="Instagram"></a>
+            <a href="#"><img src="<?= asset("img/icons/youtube.png"); ?>" alt="youtube"></a>
+          </div>
+        </div>
+      </footer>
+</body>
+
+</html>
