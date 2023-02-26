676bded40f6ca69d52575862ce87ab05511b9177
WaleedMuslem <waleedmosllm2020@gmail.com>
2023-02-26 11:41:35 +0100

    feat<404.php> : create 404 page

---
+++ pages/404.php
@@ -0,0 +1,9 @@
+<?php require_once  BASE_PATH."/views/_header.php"; ?>
+<h1>Oh no page not found</h1>
+<p>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus eligendi veritatis ex soluta magni modi tempore fugit suscipit necessitatibus asperiores recusandae in aut, ut eum dignissimos labore, sapiente nulla obcaecati.
+</p>
+<a href="<?= page_url('home'); ?>">
+    Go back to home
+</a>
+<?php require_once  BASE_PATH."/views/_footer.php"; ?>
